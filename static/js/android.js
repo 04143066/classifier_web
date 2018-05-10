5422c367f514af3633f78d5981c968fabdd0ad80
zouxiongxin <15829202176@163.com>
2018-05-10 15:59:54 +0800

    commit

---
+++ static/js/android.js
@@ -0,0 +1,12 @@
+/**
+ * 供android 端调用js代码
+ * Created by xx on 2018/5/10.
+ */
+
+function huanHang() {
+    var temp = $('.col-sm-3').text();
+    temp = temp + "<br>";
+    $('.col-sm-3').text(temp);
+}
+
+//huanHang()
